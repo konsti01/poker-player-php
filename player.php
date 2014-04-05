94a04dd656dead3d9dac68b7e0af4bcace4da309
Konstanczer Akos <akos@ux-studio.co.uk>
2014-04-05 10:24:31 +0200

    p

--- player.php
+++ player.php
@@ -6,11 +6,38 @@ class Player
 
     public function betRequest($game_state)
     {
-			//http://192.168.57.138:4567
 			extract($game_state);
 			$me = $players[$in_action];
+
+			
+			$response = null;
+			try {
+				$cards = array_merge($players[$in_action]['hole_cards'], $community_cards);
+
+				$ch = curl_init("localhost:2048");
+				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+
+				curl_setopt($ch,CURLOPT_POST, count($cards));
+				curl_setopt($ch,CURLOPT_POSTFIELDS, json_encode($cards));
+
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 1);
+				curl_setopt($ch, CURLOPT_TIMEOUT, 1);
+				$response = json_decode(curl_exec($ch), true);
+				curl_close($ch);
+				
+			} catch (Exception $e){
+				
+			}
+			//http://192.168.57.181:2048/
 			
 			$bet = $current_buy_in - $me['bet'];
+			if ($response['rank'] != null){
+				if ($response['rank'] > 3){
+					$bet += $minimum_raise * 2;
+				}
+			}
+			
       return $bet;
     }
 
