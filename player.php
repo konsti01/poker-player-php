c807f7cfd49e0908fa77e6f5e9d63b2b898029c0
Konstanczer Akos <akos@ux-studio.co.uk>
2014-04-05 13:45:45 +0200

    p

--- player.php
+++ player.php
@@ -2,12 +2,43 @@
 
 class Player {
 
-	const VERSION = "NoCo1247";
+	const VERSION = "NoCo1345";
+
+	private $_max_point = 28;
+	private $_all_in = 25;
 
 	public function betRequest($game_state) {
 		$bet = 0;
 		extract($game_state);
 		$me = $players[$in_action];
+
+		$point = $this->rank_pre_flop($me['hole_cards']);
+
+		//Akkor volt emelés
+		if ($current_buy_in > $small_blind * 2) {
+			$bet = $current_buy_in;
+		}
+
+		$jolapomvan = false;
+		// if ($point > $this->_max_point)
+		if ($point > $this->_all_in) {
+			$bet = $me['stack'] > 1000 ? 1000 : $me['stack'];
+			$jolapomvan = true;
+		} elseif ($point > $this->_max_point * 0.8) {
+			$bet = $minimum_raise * 10;
+			$jolapomvan = true;
+		} elseif ($point > $this->_max_point * 0.6) {
+			$bet = $minimum_raise * 5;
+			$bet = ($point / $this->_max_point) * $me['stack'];
+		}
+
+		if ($jolapomvan){
+			return ($bet > $current_buy_in) ? $bet : $current_buy_in;
+		}
+
+		return $bet;
+
+
 
 		$response = null;
 		if (count($community_cards) > 0) {
@@ -16,46 +47,83 @@ class Player {
 
 			if ($response['rank'] > 0) {
 				$bet = $response['rank'] * 60;
-				if ($minimum_raise > $bet){
+				if ($minimum_raise > $bet) {
 					$bet = $minimum_raise;
 				}
-				/*if ($response['rank'] > 2){
-					$bet += $minimum_raise;
-				}
-				if ($response['rank'] > 3){
-					$bet += $minimum_raise * 2;
-				}*/
+				/* if ($response['rank'] > 2){
+				  $bet += $minimum_raise;
+				  }
+				  if ($response['rank'] > 3){
+				  $bet += $minimum_raise * 2;
+				  } */
 			} else {
 				if ($me['bet'] < 50) {
 					$bet = 0;
 				}
 			}
 		} else {
-			if ($small_blind == $me['bet'] && $current_buy_in == $small_blind){
+			if ($small_blind == $me['bet'] && $current_buy_in == $small_blind) {
 				$bet = $current_buy_in;
 			}
-			
+
 			if ($me['hole_cards'][0]['rank'] == $me['hole_cards'][1]['rank']) {
 				$bet += $minimum_raise;
-				if ($this->is_high($me['hole_cards'][0]['rank'])){
+				if ($this->is_high($me['hole_cards'][0]['rank'])) {
 					$bet += $minimum_raise * 10;
 				}
 			}
 			if ($me['bet'] < 500 && $minimum_raise > 400) {
+
 				$bet = 0;
 			}
+
+			$this->rank_pre_flop($me['hole_cards']);
 		}
 
 		//http://192.168.57.181:2048/
-
 		//$bet = $current_buy_in - $me['bet'];
 
 
 		return $bet;
 	}
-	
-	private function is_high($card){
-		switch ($card){
+
+	private function rank_pre_flop($cards) {
+		$point = 0;
+		foreach ($cards as $card) {
+			$point += $this->parse_card($card['rank']);
+		}
+
+		if ($cards[0]['suit'] == $cards[1]['suit']) {
+			$point *= 1.125;
+		}
+
+		return $point;
+	}
+
+	private function parse_card($rank) {
+		if ((int) $rank > 0) {
+			$result = (int) $rank;
+		}
+
+		switch ($rank) {
+			case 'j':
+				$result = 11;
+				break;
+			case 'q':
+				$result = 12;
+				break;
+			case 'k':
+				$result = 13;
+				break;
+			case 'a':
+				$result = 14;
+				break;
+		}
+		return $result;
+	}
+
+	private function is_high($card) {
+		switch ($card) {
 			case 'k':
 			case 'q':
 			case 'j':
