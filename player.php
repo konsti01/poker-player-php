25b93ebc3b50631e583d0f206e2daba44cdb96be
Konstanczer Akos <akos@ux-studio.co.uk>
2014-04-05 11:26:01 +0200

    p

--- player.php
+++ player.php
@@ -2,22 +2,41 @@
 
 class Player
 {
-    const VERSION = "NoCo1100";
+    const VERSION = "NoCo1125";
 
     public function betRequest($game_state)
     {
 			extract($game_state);
 			$me = $players[$in_action];
-
 			
+			$response = null;
+			if (count($community_cards) > 0){
+				$cards = array_merge($players[$in_action]['hole_cards'], $community_cards);
+				$response = $this->get_rankings($cards);
+			}
+			
+			//http://192.168.57.181:2048/
+			
+			$bet = $current_buy_in - $me['bet'];
+			if ($response['rank'] != null){
+				if ($response['rank'] > 3){
+					$bet += $minimum_raise * 2;
+				} else {
+					$bet = 0;
+				}
+			}
+			fclose($stderr);
+			
+      return $bet;
+    }
+		
+		private function get_rankings($cards){
 			$response = null;
 			
 			$stderr = fopen("php://stderr", "w");
 			
 			try {
-				$cards = array_merge($players[$in_action]['hole_cards'], $community_cards);
-
-				$ch = curl_init("http://localhost:2048");
+				$ch = curl_init("http://192.168.57.181:2048");
 				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 
 				curl_setopt($ch,CURLOPT_POST, count($cards));
@@ -35,20 +54,8 @@ class Player
 				fwrite($stderr, "hello".$e->error());
 			}
 			
-			//http://192.168.57.181:2048/
-			
-			$bet = $current_buy_in - $me['bet'];
-			if ($response['rank'] != null){
-				if ($response['rank'] > 3){
-					$bet += $minimum_raise * 2;
-				} else {
-					$bet = 0;
-				}
-			}
-			fclose($stderr);
-			
-      return $bet;
-    }
+			return $response;
+		}
 
     public function showdown($game_state)
     {
