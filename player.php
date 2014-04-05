647e3686ff9ca6d93f132695122f055061673099
Konstanczer Akos <akos@ux-studio.co.uk>
2014-04-05 10:07:43 +0200

    p

--- player.php
+++ player.php
@@ -2,11 +2,13 @@
 
 class Player
 {
-    const VERSION = "NoCo";
+    const VERSION = "NoCo1007";
 
     public function betRequest($game_state)
     {
-        return 0;
+			//http://192.168.57.138:4567
+			$bet = $game_state['current_buy_in'] - $game_state['players'][$game_state['in_action']]['bet'];
+      return $bet;
     }
 
     public function showdown($game_state)
