dfce3b95289e3c75d214a7e4954c3499344924d3
Konstanczer Akos <akos@ux-studio.co.uk>
2014-04-05 11:33:01 +0200

    p

--- player.php
+++ player.php
@@ -1,63 +1,68 @@
 <?php
 
-class Player
-{
-    const VERSION = "NoCo1125";
+class Player {
 
-    public function betRequest($game_state)
-    {
-			extract($game_state);
-			$me = $players[$in_action];
-			
-			$response = null;
-			if (count($community_cards) > 0){
-				$cards = array_merge($players[$in_action]['hole_cards'], $community_cards);
-				$response = $this->get_rankings($cards);
-			}
-			
-			//http://192.168.57.181:2048/
-			
-			$bet = $current_buy_in - $me['bet'];
-			if ($response['rank'] != null){
-				if ($response['rank'] > 3){
-					$bet += $minimum_raise * 2;
-				} else {
-					$bet = 0;
-				}
-			}
-			fclose($stderr);
-			
-      return $bet;
-    }
-		
-		private function get_rankings($cards){
-			$response = null;
-			
-			$stderr = fopen("php://stderr", "w");
-			
-			try {
-				$ch = curl_init("http://localhost:2048");
-				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+	const VERSION = "NoCo1125";
 
-				curl_setopt($ch,CURLOPT_POST, count($cards));
-				curl_setopt($ch,CURLOPT_POSTFIELDS, json_encode($cards));
+	public function betRequest($game_state) {
+		extract($game_state);
+		$me = $players[$in_action];
 
-				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-				curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 1);
-				curl_setopt($ch, CURLOPT_TIMEOUT, 1);
-				$response = curl_exec($ch);
-				$error = curl_error($ch);
-				curl_close($ch);
-				fwrite($stderr, $error." - ".$response);
-				$response = json_decode($response, true);
-			} catch (Exception $e){
-				fwrite($stderr, "hello".$e->error());
-			}
-			
-			return $response;
+		$response = null;
+		if (count($community_cards) > 0) {
+			$cards['cards'] = array_merge($me['hole_cards'], $community_cards);
+			$response = $this->get_rankings($cards);
 		}
 
-    public function showdown($game_state)
-    {
-    }
+		//http://192.168.57.181:2048/
+
+		$bet = $current_buy_in - $me['bet'];
+		if ($response['rank'] != null) {
+			if ($response['rank'] > 3) {
+				$bet += $minimum_raise * 2;
+			} else {
+				$bet = 0;
+			}
+		} else {
+
+
+			if ($me['hole_cards'][0]['rank'] == $me['hole_cards'][1]['rank']) {
+				$bet += $minimum_raise;
+			}
+		}
+
+		return $bet;
+	}
+
+	private function get_rankings($cards) {
+		$response = null;
+
+		$stderr = fopen("php://stderr", "w");
+
+		try {
+			$ch = curl_init("http://localhost:2048");
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+
+			curl_setopt($ch, CURLOPT_POST, count($cards));
+			curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($cards));
+
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 1);
+			curl_setopt($ch, CURLOPT_TIMEOUT, 1);
+			$response = curl_exec($ch);
+			$error = curl_error($ch);
+			curl_close($ch);
+			fwrite($stderr, $error . " - " . $response);
+			$response = json_decode($response, true);
+		} catch (Exception $e) {
+			fwrite($stderr, "hello" . $e->error());
+		}
+		fclose($stderr);
+		return $response;
+	}
+
+	public function showdown($game_state) {
+		
+	}
+
 }
