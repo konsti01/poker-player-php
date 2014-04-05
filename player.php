a57ac6be7c5f34a7f85ffd5b8cd7fbdbc503c5c8
Konstanczer Akos <akos@ux-studio.co.uk>
2014-04-05 15:21:36 +0200

    p

--- player.php
+++ player.php
@@ -34,9 +34,6 @@ class Player {
 				$bet = $small_blind * 5;
 				$bet = ($bet > $current_buy_in) ? $bet : 0;
 			}
-			
-			$result = $bet;
-			
 		} else {
 
 			$point = $this->rank_pre_flop($me['hole_cards']);
@@ -54,66 +51,19 @@ class Player {
 				$jolapomvan = true;
 			} elseif ($point > ($this->_max_point * 0.6)) {
 				$bet = $minimum_raise * 5;
-			} else {
-				$result = 0;
 			}
 
 			if ($jolapomvan) {
-				$result = ($bet > $current_buy_in) ? $bet : $current_buy_in;
+				$bet = ($bet > $current_buy_in) ? $bet : $current_buy_in;
 				if ($nagyonjolapomvan){
-					$result *= 2;
+					$bet *= 2;
 					if($this->ketten_vagyunk($players)){
-						$result = $me['stack'];
+						$bet = $me['stack'];
 					}
 				}
 			}
 		}
-		return $result;
-
-		$response = null;
-		if (count($community_cards) > 0) {
-			$cards['cards'] = json_encode(array_merge($me['hole_cards'], $community_cards));
-			$response = $this->get_rankings($cards);
-
-			if ($response['rank'] > 0) {
-				$bet = $response['rank'] * 60;
-				if ($minimum_raise > $bet) {
-					$bet = $minimum_raise;
-				}
-				 if ($response['rank'] > 2){
-				  $bet += $minimum_raise;
-				  }
-				  if ($response['rank'] > 3){
-				  $bet += $minimum_raise * 2;
-				  }
-			} else {
-				if ($me['bet'] < 50) {
-					$bet = 0;
-				}
-			}
-		} else {
-			if ($small_blind == $me['bet'] && $current_buy_in == $small_blind) {
-				$bet = $current_buy_in;
-			}
-
-			if ($me['hole_cards'][0]['rank'] == $me['hole_cards'][1]['rank']) {
-				$bet += $minimum_raise;
-				if ($this->is_high($me['hole_cards'][0]['rank'])) {
-					$bet += $minimum_raise * 10;
-				}
-			}
-			if ($me['bet'] < 500 && $minimum_raise > 400) {
-
-				$bet = 0;
-			}
-
-			$this->rank_pre_flop($me['hole_cards']);
-		}
-
-		//http://192.168.57.181:2048/
-		//$bet = $current_buy_in - $me['bet'];
-
-		$bet = rand($current_buy_in - $me['bet'], 600);
+		
 		return $bet;
 	}
 	
