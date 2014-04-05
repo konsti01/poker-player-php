eb59100f47dd93f7cab9f311481615a0efcbb63c
anti <anti@anti-laptop>
2014-04-05 13:06:12 +0200

    poker

--- player.php
+++ player.php
@@ -2,55 +2,57 @@
 
 class Player {
 
-	const VERSION = "NoCo1247";
+	const VERSION = "NoCo1300";
 
 	public function betRequest($game_state) {
 		$bet = 0;
 		extract($game_state);
-		$me = $players[$in_action];
-
-		$response = null;
-		if (count($community_cards) > 0) {
-			$cards['cards'] = json_encode(array_merge($me['hole_cards'], $community_cards));
-			$response = $this->get_rankings($cards);
-
-			if ($response['rank'] > 0) {
-				$bet = $response['rank'] * 60;
-				if ($minimum_raise > $bet){
-					$bet = $minimum_raise;
-				}
-				/*if ($response['rank'] > 2){
-					$bet += $minimum_raise;
-				}
-				if ($response['rank'] > 3){
-					$bet += $minimum_raise * 2;
-				}*/
-			} else {
-				if ($me['bet'] < 50) {
-					$bet = 0;
-				}
-			}
-		} else {
-			if ($small_blind == $me['bet'] && $current_buy_in == $small_blind){
-				$bet = $current_buy_in;
-			}
-			
-			if ($me['hole_cards'][0]['rank'] == $me['hole_cards'][1]['rank']) {
-				$bet += $minimum_raise;
-				if ($this->is_high($me['hole_cards'][0]['rank'])){
-					$bet += $minimum_raise * 10;
-				}
-			}
-			if ($me['bet'] < 500 && $minimum_raise > 400) {
-				$bet = 0;
-			}
-		}
-
-		//http://192.168.57.181:2048/
-
-		//$bet = $current_buy_in - $me['bet'];
-
-
+//		$me = $players[$in_action];
+//
+//		$response = null;
+//		if (count($community_cards) > 0) {
+//			$cards['cards'] = json_encode(array_merge($me['hole_cards'], $community_cards));
+//			$response = $this->get_rankings($cards);
+//
+//			if ($response['rank'] > 0) {
+//				$bet = $response['rank'] * 60;
+//				if ($minimum_raise > $bet){
+//					$bet = $minimum_raise;
+//				}
+//				/*if ($response['rank'] > 2){
+//					$bet += $minimum_raise;
+//				}
+//				if ($response['rank'] > 3){
+//					$bet += $minimum_raise * 2;
+//				}*/
+//			} else {
+//				if ($me['bet'] < 50) {
+//					$bet = 0;
+//				}
+//			}
+//		} else {
+//			if ($small_blind == $me['bet'] && $current_buy_in == $small_blind){
+//				$bet = $current_buy_in;
+//			}
+//			
+//			if ($me['hole_cards'][0]['rank'] == $me['hole_cards'][1]['rank']) {
+//				$bet += $minimum_raise;
+//				if ($this->is_high($me['hole_cards'][0]['rank'])){
+//					$bet += $minimum_raise * 10;
+//				}
+//			}
+//			if ($me['bet'] < 500 && $minimum_raise > 400) {
+//				$bet = 0;
+//			}
+//		}
+//
+//		//http://192.168.57.181:2048/
+//
+//		//$bet = $current_buy_in - $me['bet'];
+//
+//
+		$bet = rand($minimum_raise, 1000);
+		
 		return $bet;
 	}
 	
